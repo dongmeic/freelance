37d05108ca8f20ed9b652c65205bd41a8fb8aaa4
dchen <dongmeic@uoregon.edu>
2020-01-05 16:34:36 -0800

    kmeans

--- weighted_overlay.R
+++ weighted_overlay.R
@@ -1,7 +1,10 @@
 library(raster)
 library(rgdal)
+library(maptools)
 
 path <- "/Volumes/dongmeic/17.Fall_2019/FSEEE/raster/"
+point <- readShapePoints("/Volumes/dongmeic/17.Fall_2019/FSEEE/terminals/CoosBay_prj.shp")
+proj4string(point) <- '+proj=lcc +lat_1=43 +lat_2=45.5 +lat_0=41.75 +lon_0=-120.5 +x_0=399999.9999984 +y_0=0 +datum=NAD83 +units=ft +no_defs +ellps=GRS80 +towgs84=0,0,0'
 
 DEM <- raster(paste0(path, "DEM10m_Reclass.tif"))
 plot(DEM, main="DEM")
@@ -23,4 +26,10 @@ ugb <- raster(paste0(path, "UGB10mRM.tif"))
 plot(ugb, main="Urban growth boundary")
 
 outraster <- DEM * 0.04 + slope * 0.12 + roads * 0.03 + landslides * 0.07 + forests * 0.17 + nature * 0.17 + lakes * 0.17 + streams * 0.09 + ugb * 0.14
+plot(outraster)
+
+rast <- raster()
+extent(rast) <- extent(DEM)
+res(rast) <- res(DEM)
+rast2 <- rasterize(point, rast, point$id, fun=mean)
 
